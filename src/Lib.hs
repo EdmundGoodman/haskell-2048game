d09a2fc0b99d56c1eb300674094a8ade8b3f1016
EdmundGoodman <egoodman3141@gmail.com>
2021-06-22 20:19:23 +0100

    Started implementing the board

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,30 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List
+
+
+data Row = MkRow [Int]
+    deriving (Eq)
+instance Show Row where
+    show (MkRow xs) = show xs
+
+data Board = MkBoard [Row]
+    deriving (Eq)
+instance Show Board where
+    show (MkBoard xss) = intercalate "\n" [show xs | MkRow xs <- xss]
+
+data Direction = L | R | U | D
+
+
+getScore :: Board -> Int
+getScore (MkBoard xss) = maximum [maximum xs | MkRow xs <- xss]
+
+hasLost :: Board -> Bool
+hasLost = undefined
+
+moveRow :: Row -> Direction -> Row
+moveRow (MkRow xs) L = undefined
+moveRow (MkRow xs) R = undefined
+
+move :: Board -> Direction -> Board
+move = undefined
